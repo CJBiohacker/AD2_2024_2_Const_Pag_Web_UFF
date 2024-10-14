bdf147301d8de6b37c1ccb9c557714ef2b798b27
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-10-14 18:18:16 -0300

    feat: questao 1 finalizada

---
+++ planos.js
@@ -0,0 +1,5 @@
+const abrePaginaPlano = (caminhoPlano) => window.open(caminhoPlano, '_blank', 'width=350,height=420');
+const fechaPaginaPlano = () => {
+    console.log("FECHA")
+    window.close();
+}
