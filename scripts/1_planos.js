f1c8b087e87bacd71247d73d40b2c3ce9a693195
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-10-15 16:11:01 -0300

    chore: organizaçao de pastas e arquivos

--- scripts/1_planos.js
+++ scripts/1_planos.js
@@ -1,5 +1,2 @@
-const abrePaginaPlano = (caminhoPlano) => window.open(caminhoPlano, '_blank', 'width=350,height=420');
-const fechaPaginaPlano = () => {
-    console.log("FECHA")
-    window.close();
-}
+const abrePaginaPlano = (caminhoPlano) => window.open(caminhoPlano, "_blank", "width=350,height=420");
+const fechaPaginaPlano = () => window.close();
