7b35dd688010cf5336c20989a8e8a799ddd4306c
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-10-15 21:36:46 -0300

    feat: questao 4 e AD finalizada

--- scripts/3_inscricao.js
+++ scripts/3_inscricao.js
@@ -2,7 +2,6 @@
 const capturarElemento = (nomeElemento) => document.querySelector(nomeElemento);
 
 const regrasMobile = (event) => {
-  console.log("🚀 ~ regrasMobile ~ event:", event);
   const tipoMobile = event.target.id;
   const totalNumeros = event.target.value.length;
   const regexpCaracteresEspeciais = /[^\w]+/g;
@@ -49,6 +48,7 @@ celular.addEventListener("blur", (event) => regrasMobile(event));
 
 // Questao 4
 const matriculado = capturarElemento("#matriculado");
+const matricula = capturarElemento("#matricula");
 const atividade = capturarElemento("#atividade");
 const plano = capturarElemento("#plano");
 const valorTotal = capturarElemento("#valor");
@@ -56,6 +56,7 @@ const botaoCalculo = capturarElemento("#botao-calculo");
 
 window.addEventListener("load", () => {
   matriculado.checked = false;
+  matricula.value = "";
   atividade.value = "vazio";
   plano.value = "vazio";
   valorTotal.value = 0;
@@ -63,29 +64,44 @@ window.addEventListener("load", () => {
 });
 
 const calcularAtividadePlano = (atividade, plano, matriculado) => {
-  if (atividade === "vazio" || plano === "vazio") {
+  if (atividade === "vazio" && plano === "vazio") {
     valorTotal.value = 0;
+    return alert("Selecione uma atividade e um plano.");
   }
 
-  if (matriculado) valorTotal.value = 80;
-  else valorTotal.value = 0;
+  if (atividade === "vazio") {
+    valorTotal.value = 0;
+    return alert("Selecione uma atividade.");
+  }
+
+  if (plano === "vazio") {
+    valorTotal.value = 0;
+    return alert("Selecione um plano.");
+  }
+
+  if (!matriculado) {
+    valorTotal.value = 80;
+  } else {
+    valorTotal.value = 0;
+    if (matricula.value.length === 0) valorTotal.value = 80;
+  }
 
   if (plano === "anual") {
     if (atividade === "academia")
-      valorTotal.value = Number(valorTotal.value) + 180;
+      valorTotal.value = Number(valorTotal.value) + 180 * 12;
     if (atividade === "nataçao")
-      valorTotal.value = Number(valorTotal.value) + 220;
+      valorTotal.value = Number(valorTotal.value) + 220 * 12;
     if (atividade === "combo")
-      valorTotal.value = Number(valorTotal.value) + 260;
+      valorTotal.value = Number(valorTotal.value) + 260 * 12;
   }
 
   if (plano === "semestral") {
     if (atividade === "academia")
-      valorTotal.value = Number(valorTotal.value) + 300;
+      valorTotal.value = Number(valorTotal.value) + 300 * 6;
     if (atividade === "nataçao")
-      valorTotal.value = Number(valorTotal.value) + 340;
+      valorTotal.value = Number(valorTotal.value) + 340 * 6;
     if (atividade === "combo")
-      valorTotal.value = Number(valorTotal.value) + 380;
+      valorTotal.value = Number(valorTotal.value) + 380 * 6;
   }
 
   if (plano === "mensal") {
@@ -97,22 +113,6 @@ const calcularAtividadePlano = (atividade, plano, matriculado) => {
       valorTotal.value = Number(valorTotal.value) + 440;
   }
 };
-
-const temMatricula = (event) => {
-  calcularAtividadePlano(atividade.value, plano.value, event.target.checked);
-};
-
-const selecionarAtividade = (evento) => {
-  calcularAtividadePlano(evento.target.value, plano.value , matriculado.checked);
-};
-
-const selecionarPlano = (evento) => {
-  calcularAtividadePlano(atividade.value, evento.target.value, matriculado.checked);
-};
-
-matriculado.addEventListener("change", (event) => temMatricula(event));
-atividade.addEventListener("change", (event) => selecionarAtividade(event));
-plano.addEventListener("change", (event) => selecionarPlano(event));
 
 botaoCalculo.addEventListener("click", (event) => {
   calcularAtividadePlano(atividade.value, plano.value, matriculado.checked);
